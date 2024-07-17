d880a48085477702a95b60e38ef9f355fa4dd22b
OfficiallyDragon <dexter@dexterwheatcroft.uk>
2024-07-17 09:36:15 +0100

    Initial Commit

---
+++ src/main/kotlin/me/officiallydragon/slimelympics/Slimelympics.kt
@@ -0,0 +1,16 @@
+package me.officiallydragon.slimelympics
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Slimelympics : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+        println("Test")
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+        println("test")
+    }
+}
